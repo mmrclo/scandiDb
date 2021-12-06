74a80e930daf0b208a257d5bb18fdc3622d1a82f
Marcelo <marcelo.kovalink@gmail.com>
2021-12-06 20:04:22 -0300

    Update delete.php

--- api/public/delete.php
+++ api/public/delete.php
@@ -2,7 +2,6 @@
 include_once '../controls/Storage.php';
 
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 header('Access-Control-Allow-Methods: DELETE');
 header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
