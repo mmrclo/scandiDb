7b0ae2e7e5711a9e162b5677468d8870daaba8b9
Marcelo <marcelo.kovalink@gmail.com>
2022-10-06 13:38:37 -0300

    Update create.php

--- api/public/create.php
+++ api/public/create.php
@@ -1,14 +1,14 @@
 <?php
 include_once '../controls/Storage.php';
 
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-header('Content-Type: application/json');
+header("Content-Type: application/json");
 
 $method = $_SERVER['REQUEST_METHOD']; 
 if ($method == "OPTIONS") {
-    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
     //header("HTTP/1.1 200 OK");
     http_response_code(200);
