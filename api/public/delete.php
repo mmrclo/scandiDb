32f6f75320b63d9d734ad4158e094f54379808b8
Marcelo <marcelo.kovalink@gmail.com>
2022-10-06 13:39:42 -0300

    Update delete.php

--- api/public/delete.php
+++ api/public/delete.php
@@ -1,13 +1,13 @@
 <?php
 include_once '../controls/Storage.php';
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: DELETE');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: DELETE");
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
 $method = $_SERVER['REQUEST_METHOD']; 
 if ($method == "OPTIONS") {
-    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
     http_response_code(200);
     die();
