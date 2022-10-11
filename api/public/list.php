401006ac9708753b5927886d130576f878e34286
Marcelo <marcelo.kovalink@gmail.com>
2022-10-11 18:25:11 -0300

    Update list.php

--- api/public/list.php
+++ api/public/list.php
@@ -2,9 +2,9 @@
 include_once '../controls/Storage.php';
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Authorization, Content-Type");
-header("Access-Control-Allow-Methods: GET");
-header("Content-Type: application/json");
+header('Access-Control-Allow-Headers: Authorization, Content-Type' );
+header('Access-Control-Allow-Methods: GET');
+header('Content-Type: application/json');
 
 $requesttype = $_SERVER['REQUEST_METHOD'];
 
